66df7ceadfc58af9245aef017a8f39c36a34e218
deustch1 <ammogus123456@gmail.com>
2026-02-27 13:00:32 -0700

    Add files via upload

---
+++ testblahblahblah/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2435607.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 277942902, 277942890, 277942895, 277942904, 277942905, 277942910, 277942906, 277942907, 277942908, 277942909, 277942911, 277942951, 277942889, 277942901, 277942893 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
